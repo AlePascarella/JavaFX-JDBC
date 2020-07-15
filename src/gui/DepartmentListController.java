4702cfffd70f4a65733d8e67372917b3d8cb8265
Alessandro Pascarella <alepascarella10@gmail.com>
2020-07-14 23:32:24 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,9 +15,14 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.service.DepartmentService;
 
 public class DepartmentListController implements Initializable {
-
+	
+	// dependencias
+	@FXML
+	private DepartmentService service;
+	
 	@FXML
 	private TableView<Department> tableViewDepartment;
 	
@@ -27,9 +35,17 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private Button btNew;
 	
+	// Carregar os departamentos na observablelist
+	private ObservableList<Department> obsList;
+	
 	@FXML
 	public void onBtNewAction() {
 		System.out.println("onBtNewAction");
+	}
+	
+	// Injetando a dependencia
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@Override
@@ -40,13 +56,24 @@ public class DepartmentListController implements Initializable {
 	
 	// Metodo para iniciar algo da minha tela
 	private void initializeNodes() {
-		tableColumnId.setCellValueFactory(new PropertyValueFactory("id")); //Iniciar o comportamento das colunas das tabelas 
-		tableColumnNome.setCellValueFactory(new PropertyValueFactory("nome"));
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("Id")); //Iniciar o comportamento das colunas das tabelas 
+		tableColumnNome.setCellValueFactory(new PropertyValueFactory<>("Name"));
 		
 		// Ajustar o tamanho da tableWiev ao tamanho da largura e altura da janela
 		Stage stage = (Stage) Main.getMainScene().getWindow(); // pegando a referencia para o stage
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty()); // pegando a view tableViewDeártment para acompanhar a janela
 		
+	}
+	
+	
+	// Metodo responsavel por acessar o servico, carregar os departamentos e jogar na observableList e depois asociar o observableList com o tableViewDepartment
+	public void updateTableView() {
+		if(service == null) { // serve para evitar erros
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList); // carregar os itens e mostrar na tela
 	}
 
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.service.DepartmentService;
 
 public class MainViewController implements Initializable {
 	
@@ -34,7 +35,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
@@ -67,6 +68,38 @@ public class MainViewController implements Initializable {
 		mainVBox.getChildren().add(mainMenu); // Adicionando todos os filhos do mainVbox
 		mainVBox.getChildren().addAll(newVBox.getChildren()); // Adicionando todos os filhos do Vbox que queremos abrir
 		
+		//.addAll adicionando uma colecao
+		
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IOExeption", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private void loadView2(String absoluteName) { // funcao para abir outra tela
+		try {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName)); // Carregar a tela
+		VBox newVBox = loader.load(); // criando objeto VBox
+		
+		// MOstrar a nova View na View principal(MainView)
+		Scene mainScene = Main.getMainScene(); // pegando a referencia da scena principal
+		
+		// Pegando os filhos (node) do VBox About e passando pros filhos do VBox no MainScene
+		VBox mainVBox =(VBox) ((ScrollPane) mainScene.getRoot()).getContent(); // pegando referencia do VBox do MainView
+		
+		// getRoot = pega o primeiro elemento da view - no caso é um ScrollPane
+		
+		
+		// Apagando todos os filhos do VBox principal e depois adicionando os filhos do Vbox principal e em seguida dos filhos do VBox que queremos abrir
+		Node mainMenu = mainVBox.getChildren().get(0); // guardando referencia para um menu - pegando a referencia do primeiro filho do VBox principal
+		mainVBox.getChildren().clear(); // limpando todos os filhos do MainVBox
+		mainVBox.getChildren().add(mainMenu); // Adicionando todos os filhos do mainVbox
+		mainVBox.getChildren().addAll(newVBox.getChildren()); // Adicionando todos os filhos do Vbox que queremos abrir
+		
+		
+		DepartmentListController controller = loader.getController();
+		controller.setDepartmentService(new DepartmentService());
+		controller.updateTableView();
 		//.addAll adicionando uma colecao
 		
 		}
